579dd26a1e14f0f5179a92464036fc864ff88bea
Maria <zhukovamaria9@gmail.com>
2024-03-03 18:13:38 +0300

    Add state change on click

--- src/components/card/cardList/CardList.jsx
+++ src/components/card/cardList/CardList.jsx
@@ -1,42 +1,21 @@
 import React from "react";
 import CardStructure from "../cardStructure/CardStructure";
+import data from "../../../data.json";
 
 function CardList() {
   return (
     <React.Fragment>
-      <CardStructure
-        name="Безлимитный 300"
-        tariff="300"
-        speed="до 10 Мбит/сек"
-        capacity="Объем включенного трафика не ограничен"
-        nameBackgroundColor="darkblue"
-        tariffBackgroundColor="darkblue"
-      />
-      <CardStructure
-        name="Безлимитный 450"
-        tariff="450"
-        speed="до 50 Мбит/сек"
-        capacity="Объем включенного трафика не ограничен"
-        nameBackgroundColor="brown"
-        tariffBackgroundColor="brown"
-      />
-      <CardStructure
-        name="Безлимитный 550"
-        tariff="550"
-        speed="до 100 Мбит/сек"
-        capacity="Объем включенного трафика не ограничен"
-        nameBackgroundColor="darkgreen"
-        tariffBackgroundColor="darkgreen"
-        size="120px"
-      />
-      <CardStructure
-        name="Безлимитный 1000"
-        tariff="1000"
-        speed="до 200 Мбит/сек"
-        capacity="Объем включенного трафика не ограничен"
-        nameBackgroundColor="darkviolet"
-        tariffBackgroundColor="darkviolet"
-      />
+      {data.map((card) => (
+        <CardStructure
+          name={card.name}
+          tariff={card.tariff}
+          speed={card.speed}
+          capacity={card.capacity}
+          nameBackgroundColor={card.nameBackgroundColor}
+          tariffBackgroundColor={card.tariffBackgroundColor}
+          size={card.size}
+        />
+      ))}
     </React.Fragment>
   );
 }
--- src/components/card/cardStructure/CardStructure.jsx
+++ src/components/card/cardStructure/CardStructure.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import styles from "./cardStructure.module.css";
+import { useState } from "react";
 
 function CardStructure(props) {
-  const nameStyle = {
-    backgroundColor: props.nameBackgroundColor,
-    height: props.size,
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
   };
+
+  const nameStyle = { backgroundColor: props.nameBackgroundColor };
   const tariffStyle = { backgroundColor: props.tariffBackgroundColor };
+  const capacity = props.capacity;
 
   return (
-    <div className={styles.container}>
-      <h4 style={nameStyle} className={styles.name}>
+    <div className={styles.container} onClick={handleClick}>
+      <h4
+        style={nameStyle}
+        className={`${styles.name} ${clicked ? styles.clicked : ""}`}
+      >
         {props.name}
       </h4>
       <section style={tariffStyle} className={styles.tariff}>
@@ -19,7 +27,7 @@ function CardStructure(props) {
         <span className={styles.conditions}> руб/мес</span>
       </section>
       <section className={styles.speed}>{props.speed}</section>
-      <footer className={styles.capacity}>{props.capacity}</footer>
+      <footer className={`${styles[capacity]}`}>{props.capacity}</footer>
     </div>
   );
 }
